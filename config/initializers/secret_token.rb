69a28600ca55ce8ca4866266b69f416718350f49
vitalyp <vitalyp@softwareplanet.uk.com>
2012-05-19 22:44:35 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoku::Application.config.secret_token = 'cd1486344dd74e391389b4fcf6c2a11d3c045ee47063b6ed247379bfda823835eb02dfbf557c30495735d59a66fee49c39114725e6829d7a637e94827c339f3d'
